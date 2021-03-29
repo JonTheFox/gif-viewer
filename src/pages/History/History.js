a3210cdd831a3cf6cb45c5ba342dc7715adb7ac0
Jonathan Weiss <weissjon@gmail.com>
2021-03-30 01:43:34 +0300

    make history items link to search them

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import SearchHistoryState from "../../store/SearchHistoryState.js";
-import { useRecoilState } from "recoil";
-
+import PageState from "../../store/PageState.js";
+import SearchState from "../../store/SearchState.js";
+import { AppContext } from "../../store/AppContext.js";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import { useHistory } from "react-router-dom";
 
 import localStorageKeys from "../../constants/localstorageKeys.js";
 
@@ -13,8 +16,14 @@ const History = (props) => {
 	const [searchHistory, setSearchHistory] = useRecoilState(
 		SearchHistoryState
 	);
+	const [appUtils] = useContext(AppContext);
+	const { navigateTo } = appUtils;
+	const setPage = useSetRecoilState(PageState);
+	const setSearchQuery = useSetRecoilState(SearchState);
+	const history = useHistory();
 
 	useEffect(() => {
+		//update the search history in the browser's local storage
 		window.localStorage.setItem(
 			localStorageKeys.searchHistory,
 			JSON.stringify(searchHistory)
@@ -30,7 +39,15 @@ const History = (props) => {
 			<List component="nav" aria-label="history search terms">
 				{searchHistory?.map((query, i) => {
 					return (
-						<ListItem button>
+						<ListItem
+							button
+							key={query}
+							onClick={() => {
+								setPage(1);
+								setSearchQuery(query);
+								navigateTo("/main", history);
+							}}
+						>
 							<ListItemText primary={query} key={query} />
 						</ListItem>
 					);
